41ca4a75b90e6dc8131eadf7fd7724aa8e5e5357
Mandy <yymgu@edu.uwaterloo.ca>
2018-10-14 14:41:40 -0400

    added vector addition test

--- word2vec.py
+++ word2vec.py
@@ -2,14 +2,14 @@ import wikipedia
 import nltk 
 from gensim.models import Word2Vec
 
-chess = wikipedia.page("chess").content
+chess = wikipedia.page("Chess").content
 
 # split our document into sentences
 sentences = nltk.sent_tokenize(chess) 
 
 length = len(sentences)
 
-stopwords = set(nltk.corpus.stopwords.words('english'))
+stopwords = set(nltk.corpus.stopwords.words("english"))
 
 for i in range(0, length): 
     
@@ -32,3 +32,8 @@ model.similarity("rook", "knight")
 # Finds the top n most similar words 
 
 model.similar_by_word("king",10)
+
+# Find word using vector addition. Does opponent + checkmate = lose? 
+
+opponent_checkmate = model.wv["opponent"] + model.wv["checkmate"] # add the vectors for king and checkmate
+model.most_similar(positive = [opponent_checkmate], topn=10) # find most similar word to vector
