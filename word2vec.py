ee921bd52604019807d64f90eccd4e92196abf14
Mandy <yymgu@edu.uwaterloo.ca>
2018-10-14 09:59:57 -0400

    added script to create own word2vec model

---
+++ word2vec.py
@@ -0,0 +1,32 @@
+import wikipedia
+import nltk 
+from gensim.models import Word2Vec
+
+chess = wikipedia.page("chess").content
+
+# split our document into sentences
+sentences = nltk.sent_tokenize(chess) 
+
+length = len(sentences)
+
+stopwords = set(nltk.corpus.stopwords.words('english'))
+
+for i in range(0, length): 
+    
+    # further tokenize our sentences
+    temp = nltk.word_tokenize(sentences[i])
+    
+    # removing stop words, non-alpabetical tokens and converting to lower case 
+    sentences[i] = [word.lower() for word in temp if word not in stopwords and word.isalpha()]    
+
+model = Word2Vec(sentences, size=100, window=5)
+
+# Exploring the model 
+
+# Measures the similarity between words using cosine similarity 
+
+model.similarity("rook", "knight") 
+
+# Finds the top n most similar words 
+
+model.similar_by_word("king",10)
